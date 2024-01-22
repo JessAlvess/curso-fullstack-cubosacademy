0c42ce6839711819097e5da767f39a7edca5ff72
Jessé Alves <jessealvespr@outlook.com>
2024-01-22 19:18:21 -0300

    feat: realizado o exercício da aula

---
+++ m01/semana1/seg/script.js
@@ -0,0 +1,7 @@
+addEventListener("click", function(e){
+    if (e.target.classList.contains("button")) {
+        console.log("Hello World and Hello cubos students!!");
+    } else {
+        console.log("Please, click in the button");
+    }
+})
