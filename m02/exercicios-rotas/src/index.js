c41992542a4d4ebd2036a176c365275b3e06c481
Jessé Alves <jessealvespr@outlook.com>
2024-04-01 21:27:05 -0300

    feat: exercicio 2

--- m02/exercicios-rotas/src/index.js
+++ m02/exercicios-rotas/src/index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const { listarCarros, filtrarCarros } = require('./controladores/controladores')
 
+app.use((req, res, next) => {
+    const { senha } = req.query
+    if (senha) {
+        senha === '123' ? next() : res.send('Digite a senha correta')
+    } else {
+        res.send('Digite a senha para acessar o servidor')
+    }
+})
+
 app.get('/carros', listarCarros)
 app.get('/carros/:id', filtrarCarros)
 
