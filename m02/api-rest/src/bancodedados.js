1379910c9e9757d176ef6ab50146ab58f78f8948
Jessé Alves <jessealvespr@outlook.com>
2024-04-03 21:06:27 -0300

    feat: API rest

---
+++ m02/api-rest/src/bancodedados.js
@@ -0,0 +1,19 @@
+const bancodedados = {
+    identificadorInstrutor: 13,
+    instrutores: [
+    { id: 1, nome: 'João', email: 'joao@email.com', status: true },
+    { id: 2, nome: 'Maria', email: 'maria@email.com', status: true },
+    { id: 3, nome: 'Pedro', email: 'pedro@email.com', status: false },
+    { id: 4, nome: 'Ana', email: 'ana@email.com', status: true },
+    { id: 5, nome: 'Carlos', email: 'carlos@email.com', status: false },
+    { id: 6, nome: 'Mariana', email: 'mariana@email.com', status: true },
+    { id: 7, nome: 'Lucas', email: 'lucas@email.com', status: false },
+    { id: 8, nome: 'Julia', email: 'julia@email.com', status: true },
+    { id: 9, nome: 'Fernando', email: 'fernando@email.com', status: true },
+    { id: 10, nome: 'Laura', email: 'laura@email.com', status: false },
+    { id: 11, nome: 'Roberto', email: 'roberto@email.com', status: true },
+    { id: 12, nome: 'Patrícia', email: 'patricia@email.com', status: false }
+    ]
+};
+
+module.exports = bancodedados
